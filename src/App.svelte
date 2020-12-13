f626675208bcd37109874f898bdc2b8a296827da
Rosostolato <eduardo.rosostolato@gmail.com>
2020-12-13 19:00:33 -0300

    initial create

---
+++ src/App.svelte
@@ -0,0 +1,10 @@
+<script lang="ts">
+import Table from "./Table.svelte";
+</script>
+
+<style>
+</style>
+
+<main>
+	<Table />
+</main>
---
+++ src/Table.svelte
@@ -0,0 +1,31 @@
+<script lang="ts">
+  import _ from 'lodash'
+
+  const game: number[] = _.fill(Array(64), 0)
+</script>
+
+<style>
+  .table {
+    width: 650px;
+    height: 640px;
+    display: grid;
+    grid-template-rows: repeat(8, 1fr);
+    grid-template-columns: repeat(8, 1fr);
+  }
+
+  .cell:nth-child(2n + 1) {
+    background-color: #AF8460;
+  }
+
+  .cell:nth-child(2n) {
+    background-color: #E8D2AF;
+  }
+</style>
+
+<div class="table">
+  {#each game as cell, i}
+    <div class={`cell row-${Math.floor(i / 8) + 1}`}>
+      {cell}
+    </div>
+  {/each}
+</div>
