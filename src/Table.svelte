fc83eff3b0a01f93df975614726fbbe839b536ca
Rosostolato <eduardo.rosostolato@gmail.com>
2020-12-25 18:48:29 -0300

    game is playable

--- src/Table.svelte
+++ src/Table.svelte
@@ -10,58 +10,111 @@
     SelectMove,
   }
 
-  let turn: Player
-  let game: Player[]
-  let state: TurnState
   let selectedPiece: number | null
+  let currPlayer: Player
+  let squares: Player[]
+  let turnState: TurnState
+  let playerEats: Record<Player, number>
 
   resetGame()
 
   function resetGame() {
     selectedPiece = null
-    state = TurnState.SelectPiece
-    turn = Player.Red
+    turnState = TurnState.SelectPiece
+    currPlayer = Player.Red
+    playerEats = {
+      [Player.Red]: 0,
+      [Player.Black]: 0,
+      [Player.Empty]: 0,
+    }
 
-    game = Array.from(Array(8 * 8)).map((_, i) => {
-      const row = Math.floor(i / 8)
+    squares = Array(8 * 8)
+      .fill(0)
+      .map((_, i) => {
+        const row = Math.floor(i / 8)
 
-      if (row < 3 && isGameCell(i)) {
-        return Player.Black
-      }
+        if (row < 3 && isValidSquare(i)) {
+          return Player.Black
+        }
 
-      if (row > 4 && isGameCell(i)) {
-        return Player.Red
-      }
+        if (row > 4 && isValidSquare(i)) {
+          return Player.Red
+        }
 
-      return Player.Empty
-    })
+        return Player.Empty
+      })
   }
 
-  function onSelect(i: number) {
-    switch (state) {
+  function onSelect(pos: number) {
+    switch (turnState) {
       case TurnState.SelectPiece:
-        if (game[i] === turn) {
-          state = TurnState.SelectMove
-          selectedPiece = i
+        1
+        if (squares[pos] === currPlayer) {
+          turnState = TurnState.SelectMove
+          selectedPiece = pos
         }
         break
 
       case TurnState.SelectMove:
-        if (selectedPiece && isGameCell(i) && game[i] === Player.Empty) {
-          game[i] = turn
-          game[selectedPiece] = Player.Empty
-          turn = turn === Player.Red ? Player.Black : Player.Red
-          state = TurnState.SelectPiece
+        if (!selectedPiece) {
+          turnState = TurnState.SelectPiece
+          return
+        }
+
+        if (validateMove(selectedPiece, pos)) {
+          currPlayer = currPlayer === Player.Red ? Player.Black : Player.Red
+          turnState = TurnState.SelectPiece
           selectedPiece = null
         }
         break
     }
   }
 
-  function isGameCell(i: number) {
-    const row = Math.floor(i / 8)
-    const cell = i % 8
+  function validateMove(sourcePos: number, destPos: number) {
+    if (!isValidSquare(destPos) || squares[destPos] !== Player.Empty) {
+      return false
+    }
+
+    const diff = destPos - sourcePos
+    const move = isRounded(diff / 7) ? 7 : 9
+    const distance = diff / move
+    const signal = distance / Math.abs(distance)
+    const dir = signal * move
+
+    if (
+      !isRounded(distance) ||
+      Math.abs(distance) > 2 ||
+      (distance > 0 && currPlayer === Player.Red) ||
+      (distance < 0 && currPlayer === Player.Black)
+    ) {
+      return false
+    }
+
+    // Eat move
+    if (Math.abs(distance) === 2) {
+      const otherPlayer = currPlayer === Player.Red ? Player.Black : Player.Red
+
+      if (squares[sourcePos + dir] !== otherPlayer) {
+        return false
+      }
+
+      squares[sourcePos + dir] = Player.Empty
+      playerEats[currPlayer]++
+    }
+
+    squares[destPos] = currPlayer
+    squares[selectedPiece!] = Player.Empty
+    return true
+  }
+
+  function isValidSquare(pos: number) {
+    const row = Math.floor(pos / 8)
+    const cell = pos % 8
     return ((row % 2) + cell) % 2 === 1
+  }
+
+  function isRounded(num: number) {
+    return Math.round(num) / num === 1
   }
 </script>
 
@@ -111,10 +164,18 @@
       }
     }
   }
+
+  .info {
+    margin-top: 20px;
+
+    span {
+      margin-left: 10px;
+    }
+  }
 </style>
 
 <div class="table">
-  {#each game as cell, i}
+  {#each squares as cell, i}
     <div
       class={`cell cell-${cell} row-${Math.floor(i / 8) + 1}`}
       class:selected={selectedPiece === i}
@@ -122,4 +183,10 @@
       <div class="piece" />
     </div>
   {/each}
+</div>
+
+<div class="info">
+  <button on:click={() => resetGame()}>Reset Game</button>
+  <span>Player Red: {playerEats[Player.Red]}</span>
+  <span>Player Black: {playerEats[Player.Black]}</span>
 </div>
